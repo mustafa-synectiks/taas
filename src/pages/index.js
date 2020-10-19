f0337c15e1b2ef2673e110f1508a54ace45abe8b
mustafa-synectiks <mustafa.mohammed@synectiks.com>
2020-10-19 18:58:42 +0530

    Adding Nelify Cms in config js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,6 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-) 
+)
 
 export default IndexPage
